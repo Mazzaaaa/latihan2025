03f87a532b18a285fa51c3ee54803dedc0ea2e14
Azam <ryuoffc@gmail.com>
2025-05-07 14:17:17 +0700

    Halaman Profil

---
+++ profil.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <h1>Situs Milik Ku</h1>
+	<hr>
+	  <a href="welcome.php">Welcome</a>
+	  <a href="profil.php">Profil</a>
+	  <a href="portofolio.php">Portofolio</a>
+	<hr>
+	<h1>Halaman Profil</h1>
+  </body>
+</html>
